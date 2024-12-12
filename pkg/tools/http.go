470535e4c64a2734a04d6657962dc3e8db738cf0
ifooth <thezero12@hotmail.com>
2024-12-12 15:26:16 +0800

    fix: publish event failed when credential delete

--- pkg/tools/http.go
+++ pkg/tools/http.go
@@ -132,7 +132,12 @@ func respToCurl(resp *http.Response, st time.Time) string {
 				// 重新填充 Body，以便后续可以继续读取
 				resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
-				respMsg += fmt.Sprintf(string(bodyBytes))
+				// 最大打印 1024 个字符
+				body := string(bodyBytes)
+				if len(body) > 1024 {
+					body = fmt.Sprintf("%s...(Total %s)", body[:1024], humanize.Bytes(uint64(len(body))))
+				}
+				respMsg += body
 			}
 		} else {
 			respMsg += "(io.Reader)"
@@ -158,8 +163,7 @@ func (t *curlLogTransport) RoundTrip(req *http.Request) (*http.Response, error) 
 	if err != nil {
 		klog.Infof("[%s] RESP: [err] %s", rid, err)
 	} else {
-		// klog.Infof("[%s] RESP: %s", rid, respToCurl(resp, st))
-		fmt.Println("leijioamin", st)
+		klog.Infof("[%s] RESP: %s", rid, respToCurl(resp, st))
 	}
 
 	return resp, err
