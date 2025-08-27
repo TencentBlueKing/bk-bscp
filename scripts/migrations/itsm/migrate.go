2a1dcb2ef7a036aff9941653373378223838fc24
Ambition <918632536@qq.com>
2025-08-27 17:24:27 +0800

    fix: 上下文中新增tenantID

--- scripts/migrations/itsm/migrate.go
+++ scripts/migrations/itsm/migrate.go
@@ -56,6 +56,7 @@ func InitServices(ctx context.Context, tenantID string) error {
 
 	itsm := itsm.NewITSMService()
 	// 通过 workflow_keys 获取 activity_key
+	ctx = context.WithValue(context.Background(), constant.BkTenantID, tenantID)
 	workflow, err := itsm.ListWorkflow(ctx, api.ListWorkflowReq{
 		WorkflowKeys: resp.CreateApproveItsmWorkflowID.Value,
 	})
