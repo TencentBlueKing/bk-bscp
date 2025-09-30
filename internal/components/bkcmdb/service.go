341fbbd5580d77f345ca89c2889619cdf3dcffff
xiaolnwang <xiaolnwang@tencent.com>
2025-09-30 18:12:12 +0800

    fix lint

--- internal/components/bkcmdb/service.go
+++ internal/components/bkcmdb/service.go
@@ -30,11 +30,14 @@ type Service interface {
 	// ListBizHosts 查询业务下的主机
 	ListBizHosts(ctx context.Context, req *ListBizHostsRequest) (*CMDBResponse[CMDBListData[HostInfo]], error)
 	// WatchHostResource 监听主机资源变化
-	WatchHostResource(ctx context.Context, req *WatchResourceRequest) (*CMDBResponse[WatchResourceData[HostDetail]], error)
+	WatchHostResource(ctx context.Context, req *WatchResourceRequest) (
+		*CMDBResponse[WatchResourceData[HostDetail]], error)
 	// WatchHostRelationResource 监听主机关系资源变化
-	WatchHostRelationResource(ctx context.Context, req *WatchResourceRequest) (*CMDBResponse[WatchResourceData[HostRelationDetail]], error)
+	WatchHostRelationResource(ctx context.Context, req *WatchResourceRequest) (
+		*CMDBResponse[WatchResourceData[HostRelationDetail]], error)
 	// FindHostBizRelations 查询主机业务关系信息
-	FindHostBizRelations(ctx context.Context, req *FindHostBizRelationsRequest) (*FindHostBizRelationsResponse, error)
+	FindHostBizRelations(ctx context.Context, req *FindHostBizRelationsRequest) (
+		*FindHostBizRelationsResponse, error)
 }
 
 // New cmdb service
