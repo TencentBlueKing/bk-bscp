b79c081e2ea0053e67ce6aa137d15aaa8640e071
silenceper <silenceper@gmail.com>
2025-11-12 17:50:21 +0800

    fix lint error

--- render/errors.go
+++ render/errors.go
@@ -1,12 +1,14 @@
-// * Tencent is pleased to support the open source community by making Blueking Container Service available.
-//  * Copyright (C) 20\d\d THL A29 Limited, a Tencent company. All rights reserved.
-//  * Licensed under the MIT License (the "License"); you may not use this file except
-//  * in compliance with the License. You may obtain a copy of the License at
-//  * http://opensource.org/licenses/MIT
-//  * Unless required by applicable law or agreed to in writing, software distributed under
-//  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-//  * either express or implied. See the License for the specific language governing permissions and
-//  * limitations under the License.
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package render
 
@@ -36,6 +38,7 @@ var (
 )
 
 // RenderError represents a rendering error with details
+// nolint:revive
 type RenderError struct {
 	Op     string // operation that failed
 	Err    error  // underlying error
--- render/types.go
+++ render/types.go
@@ -1,16 +1,19 @@
-// * Tencent is pleased to support the open source community by making Blueking Container Service available.
-//  * Copyright (C) 20\d\d THL A29 Limited, a Tencent company. All rights reserved.
-//  * Licensed under the MIT License (the "License"); you may not use this file except
-//  * in compliance with the License. You may obtain a copy of the License at
-//  * http://opensource.org/licenses/MIT
-//  * Unless required by applicable law or agreed to in writing, software distributed under
-//  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-//  * either express or implied. See the License for the specific language governing permissions and
-//  * limitations under the License.
+/*
+ * Tencent is pleased to support the open source community by making Blueking Container Service available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package render
 
 // RenderInput represents the input data for template rendering
+// nolint:revive
 type RenderInput struct {
 	// Template is the Mako template content
 	Template string `json:"template"`
@@ -19,6 +22,7 @@ type RenderInput struct {
 }
 
 // RenderOutput represents the output of template rendering
+// nolint:revive
 type RenderOutput struct {
 	// Result is the rendered content
 	Result string
